a88106b01040a1579ce36c041cde5b4f6d205006
Ekaterina Andriushova <ekaterinaandriushova@gmail.com>
2021-02-03 14:05:57 -0500

    1part

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
@@ -101,12 +101,12 @@ class BurgerBuilder extends Component {
 
     return (
        <Aux>
-          <Modal show={this.state.purchasing}>
+          <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
             <OrderSummary
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}/>
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+              purchaseCancelled={this.purchaseCancelHandler}
+              purchaseContinued={this.purchaseContinueHandler}/>
           </Modal>
           <Burger ingredients ={this.state.ingredients}/>
           <BuildControls
